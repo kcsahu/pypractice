a9dbee73336063ba6cf9725eb4ccfba8b5421836
kcsahu <kailashsahu.official@gmail.com>
2025-10-10 06:40:08 +0530

    ds algo

--- linkedlist/linked_list_ds.py
+++ linkedlist/linked_list_ds.py
@@ -1,3 +1,5 @@
+from contextlib import nullcontext
+
 from linkedlist.list_util import ListNode, ListUtil
 
 def detect_cycle(head: ListNode)-> bool:
@@ -44,8 +46,33 @@ def remove_nth_node(head: ListNode, n: int)->ListNode:
     node2.next = node2.next.next
     return result.next
 
+def rotate_right(head: ListNode, k: int)-> ListNode:
+    len = 1;
+    node1 = head
+    while node1.next:
+        node1 = node1.next
+        len += 1
+    k = k % len
+    if k ==0:
+        return head
+    node1 = head
+    node2 = head
+    while node1.next:
+        node1 = node1.next
+        if k > 0:
+            k -= 1
+        else:
+            node2 = node2.next
+    result = node2.next
+    node2.next = None
+    node1.next = head
+    return result
+
 
 if __name__=="__main__":
+    head = ListUtil.create_linked_list([1,2,3,4,5,6,7])
+    head = rotate_right(head, 2)
+    ListUtil.print_linklist(head, "Rotated: ")
     head = ListUtil.create_linked_list([1,2,3,4,5,6,7])
     head = remove_nth_node(head, 2)
     ListUtil.print_linklist(head, "result")
