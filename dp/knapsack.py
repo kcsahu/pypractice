86062a97d332d8e6b98fd6e4dcee2047a65cf7ed
kcsahu <kailashsahu.official@gmail.com>
2025-12-13 12:58:24 +0530

    arrays

--- dp/knapsack.py
+++ dp/knapsack.py
@@ -1,10 +1,13 @@
 
 def knapsack(values: list, capacity: int, wts: list)-> int:
-    dp = [0 for _ in range(capacity + 1)]
-    for index, value in enumerate(values):
-        for w in range(capacity, wts[index] -1, -1):
-            dp[w] = max(dp[w], value + dp[w - wts[index]])
+    dp = [0 for i in range(capacity + 1)]
+    for ind, val in enumerate(values):
+        for w in range(capacity, wts[ind] - 1, -1):
+            dp[w] = max(dp[w], val + dp[w - wts[ind]])
     return dp[capacity]
+
+
+
 
 if __name__== "__main__":
     res = knapsack([1, 8, 22, 28, 18], 11, [1, 3, 6, 7, 5])
