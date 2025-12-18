c122b1c8a35baa7535f8849e469676c517dd203d
kcsahu <kailashsahu.official@gmail.com>
2025-12-18 23:28:52 +0530

    ds algo

--- simple/fibonacci.py
+++ simple/fibonacci.py
@@ -1,3 +1,4 @@
+from collections import deque
 from functools import lru_cache
 
 @lru_cache(10000)
@@ -8,6 +9,25 @@ def fibonacci(n: int) -> int:
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
 
+def fibonacci_DFS(n:int)->int:
+    if n == 0:
+        return 0
+    if n== 1:
+        return 1
+    dq = deque()
+    dq.append(0)
+    dq.append(1)
+    while n > 1:
+        current = dq.pop()
+        prev = dq.pop()
+        dq.append(current)
+        dq.append(current + prev)
+        n -= 1
+    return dq.pop()
+
+
 if __name__ == "__main__":
-    val = fibonacci(8)
+    val = fibonacci(10)
     print(val)
+
+    print(fibonacci_DFS(1000))
