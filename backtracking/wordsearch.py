196e794fb5017b44f9c3f6ae2c62d4dbbccbd63c
kcsahu <kailashsahu.official@gmail.com>
2025-12-27 12:45:37 +0530

    ds algo

--- backtracking/wordsearch.py
+++ backtracking/wordsearch.py
@@ -1,40 +1,50 @@
+# Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+#
+# The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally
+# or vertically neighboring. The same letter cell may not be used more than once.
+
+# Input: board = [["A","B","C","E"],
+#                 ["S","F","C","S"],
+#                 ["A","D","E","E"]],
+# word = "ABCCED"
+# Output: true
 from typing import List
 
 
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        if not board or len(board) == 0 or not word or len(word) == 0:
+def exist(board: List[List[str]], word: str) -> bool:
+    def backtrack(row: int, col: int, index: int = 0) -> bool:
+        if index == len(word):
+            return True
+        if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]) or board[row][col] != word[index]:
             return False
+        tmp = board[row][col]
+        board[row][col] = '$'
+        result = backtrack(row + 1, col, index + 1) or \
+                 backtrack(row - 1, col, index + 1) or \
+                 backtrack(row, col + 1, index + 1) or \
+                 backtrack(row, col - 1, index + 1)
+        board[row][col] = tmp
+        return result
+
+    if board and word:
         rows = len(board)
         cols = len(board[0])
         for row in range(rows):
             for col in range(cols):
-                if board[row][col] == word[0] and self.solve(row, col, board, word, 0):
+                if board[row][col] == word[0] and backtrack(row, col):
                     return True
-        return False
-
-    def solve(self, row: int, col: int, board: List[List[str]], word: str, index: int) -> bool:
-        if index == len(word):
-            return True
-        if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]) or \
-               word[index] != board[row][col]:
-            return False
-
-        tmp = board[row][col]
-        board[row][col] = '$'
-        result = self.solve(row + 1, col, board, word, index + 1) or \
-                 self.solve(row - 1, col, board, word, index + 1) or \
-                 self.solve(row, col + 1, board, word, index + 1) or \
-                 self.solve(row, col - 1, board, word, index + 1)
-        board[row][col] = tmp
-        return result
+    return False
 
 
 if __name__ == "__main__":
-    obj = Solution()
-    res = obj.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")
+    res = exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")
     print(res)
     assert res
-    res = obj.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "SEE")
+
+    res = exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "SEE")
     print(res)
     assert res
+
+    res = exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCB")
+    print(res)
+    assert not res
