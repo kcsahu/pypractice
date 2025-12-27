6b88b95621ea42d95b18721f5327d53880bbffd0
kcsahu <kailashsahu.official@gmail.com>
2025-12-27 21:37:52 +0530

    ds algo

--- backtracking/wordsearchII.py
+++ backtracking/wordsearchII.py
@@ -1,70 +1,68 @@
-from typing import List
-
 
 class TrieNode:
-    child: dict[str, 'TrieNode'] = None
-    is_end: bool
-    word: str
 
     def __init__(self):
-        self.child = dict()
+        self.child = {}
         self.is_end = False
-
+        self.word: str = None
 
 class Solution:
-    root: TrieNode = TrieNode()
-
-    def build_trie(self, words: list):
+    def __init__(self):
+        self.root = TrieNode()
+    def __build_trie(self, words: list[str]):
         for word in words:
-            node = self.root
+            node: TrieNode = self.root
             for w in word:
                 next_node = node.child.get(w, None)
                 if not next_node:
                     next_node = TrieNode()
-                node.child[w] = next_node
+                    node.child[w] = next_node
                 node = next_node
             node.is_end = True
             node.word = word
 
-    def findWords(self, board: List[List[str]], words: List[str]):
-        self.build_trie(words)
-        rows = len(board)
-        cols = len(board[0])
+    def findWords(self, board: list[list[str]], words: list[str])-> list:
         result = []
-        for row in range(rows):
-            for col in range(cols):
-                if board[row][col] in self.root.child.keys():
-                    self.solve(row, col, board, self.root, result)
+        def backtrack(row: int, col: int, node: TrieNode):
+            if node and node.is_end:
+                node.is_end = False
+                result.append(node.word)
+            if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]):
+                return
+            tmp = board[row][col]
+            board[row][col] = '#'
+            node = node.child.get(tmp, None)
+            if node:
+                backtrack(row + 1, col, node)
+                backtrack(row - 1, col, node)
+                backtrack(row, col + 1, node)
+                backtrack(row, col - 1, node)
+            board[row][col] = tmp
+
+        if board and words:
+            self.__build_trie(words)
+            rows = len(board)
+            cols = len(board[0])
+            for row in range(rows):
+                for col in range(cols):
+                    if board[row][col] in self.root.child.keys():
+                        backtrack(row, col, self.root)
         return result
-
-    def solve(self, row, col, board, node: TrieNode, result: list):
-        if node.is_end:
-            result.append(node.word)
-            node.is_end = False
-            return True
-        if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]):
-            return False
-        tmp = board[row][col]
-        board[row][col] = '$'
-        node = node.child.get(tmp, None)
-        is_found = False
-        if node:
-            is_found = (self.solve(row + 1, col, board, node, result)
-                        or self.solve(row - 1, col, board, node, result)
-                        or self.solve(row, col + 1, board, node, result)
-                        or self.solve(row, col - 1, board, node, result))
-        board[row][col] = tmp
-        return is_found
-
 
 if __name__ == "__main__":
     obj = Solution()
     res = obj.findWords([["o", "a", "a", "n"], ["e", "t", "a", "e"], ["i", "h", "k", "r"], ["i", "f", "l", "v"]],
-                        ["oath", "pea", "eat", "rain"])
+                         ["oath", "pea", "eat", "rain"])
     print(res)
     assert ['oath', 'eat'] == res
 
-    res = obj.findWords([["o", "a", "b", "n"], ["o", "t", "a", "e"], ["a", "h", "k", "r"], ["a", "f", "l", "v"]],
-                        ["oa", "oaa"])
+    res = obj.findWords([["a","b"],["c","d"]], ["abcb"])
     print(res)
-    assert ['oa', 'oaa'] == res
+    assert [] == res
+
+    res = obj.findWords([["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]],
+                        # ["hklf","hf"]
+                        ["oath","pea","eat","rain","hklf", "hf"]
+                        )
+    print(res)
+    assert res == ['oath', 'eat', 'hf', 'hklf']
