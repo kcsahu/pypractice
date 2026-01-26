8b93c6df3f977c3561ccfe744d0ae1bf6aea97d0
kcsahu <kailashsahu.official@gmail.com>
2026-01-26 19:52:00 +0530

    Added

---
+++ graph/reconstruct_itinerary.py
@@ -0,0 +1,22 @@
+from collections import defaultdict,deque
+import heapq
+def findItenary(tickets: list[list[str]]) -> list[str]:
+    if not tickets:
+      return []
+    graph = defaultdict(list)
+    for src, dest in tickets:
+       heapq.heappush(graph[src], dest)
+    route = list()
+    def dfs(node):
+       while graph[node]:
+          next = heapq.heappop(graph[node])
+          dfs(next)
+       route.append(node)
+    dfs('JFK')
+    return route[::-1]
+    
+
+if __name__ =="__main__":
+   tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+   res = findItenary(tickets)
+   print(res)
